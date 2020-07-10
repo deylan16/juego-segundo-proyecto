8b37d4e297e7ec3809d15934fde6f8a77669e843
deylan <deylan6@gmail.com>
2020-07-10 11:10:42 -0600

    se quito el parpadeo pero los disparos salen seguidos

--- cuadros.py
+++ cuadros.py
@@ -1,5 +1,7 @@
 import pygame
 from pygame.locals import *
+from time import *
+
 
 #botones de los cuadros
 class Boton_cuadros(pygame.sprite.Sprite):
@@ -9,6 +11,7 @@ class Boton_cuadros(pygame.sprite.Sprite):
         self.y = y
         self.rect = self.imagen_actual.get_rect()
         self.rect.left,self.rect.top = (x,y)
+        self.lista_disparo =[]
     def cambio(self,imagen):
         self.imagen_actual = imagen
     def estado_get(self):
@@ -17,6 +20,11 @@ class Boton_cuadros(pygame.sprite.Sprite):
         return self.x
     def coordsy_get(self):
         return self.y
+    def disparos(self,x,y,img):
+        if self.imagen_actual  == cuadro_o_con_water_rooks:
+            bala = proyectil(x,y,img)
+            self.lista_disparo.append(bala)
+        
     def seleccion(self,pantalla,cursor):
         pantalla.blit(self.imagen_actual,self.rect)
         
@@ -98,3 +106,33 @@ cuadro_8_4= Boton_cuadros(mesa[8][4] ,173,384)
 
 
 ################################################3
+c = [cuadro_0_0,cuadro_0_1,cuadro_0_2,cuadro_0_3,cuadro_0_4,cuadro_1_0,cuadro_1_1,cuadro_1_2,cuadro_1_3,cuadro_1_4,cuadro_2_0,cuadro_2_1,cuadro_2_2,
+    cuadro_2_3,cuadro_2_4,cuadro_3_0,cuadro_3_1,cuadro_3_2,cuadro_3_3,cuadro_3_4,cuadro_4_0,cuadro_4_1,cuadro_4_2,cuadro_4_3,cuadro_4_4,cuadro_5_0,
+    cuadro_5_1,cuadro_5_2,cuadro_5_3,cuadro_5_4,cuadro_6_0,cuadro_6_1,cuadro_6_2,cuadro_6_3,cuadro_6_4,cuadro_7_0,cuadro_7_1,cuadro_7_2,cuadro_7_3,
+    cuadro_7_4,cuadro_8_0,cuadro_8_1,cuadro_8_2,cuadro_8_3,cuadro_8_4]
+
+
+#imagenes de los proyectiles
+bola_de_agua = pygame.image.load("bala.png")
+
+
+
+
+
+y0 = c[0].coordsy_get()
+x0 = c[0].coordsx_get()
+
+
+class proyectil(pygame.sprite.Sprite):
+    def __init__(self,x,y,imagen):
+        self.imagen = imagen
+        self.rect = self.imagen.get_rect()
+        self.velocidad = -1
+        self.rect.top = y
+        self.rect.left = x
+
+    def direccion(self):
+        self.rect.top = self.rect.top - self.velocidad
+    def disparo(self,ventana):
+        ventana.blit(self.imagen,self.rect)
+         
--- proyecto.py
+++ proyecto.py
@@ -12,27 +12,7 @@ pygame.init()
 # creamos la ventana y le indicamos un titulo:
 ventana = pygame.display.set_mode((267,440))
 pygame.display.set_caption("juego")
-##########################################3#######
-#imagenes de los proyectiles
-agua = pygame.image.load("bala.png")
-#proyectiles
-c = [cuadro_0_0,cuadro_0_1,cuadro_0_2,cuadro_0_3,cuadro_0_4,cuadro_1_0,cuadro_1_1,cuadro_1_2,cuadro_1_3,cuadro_1_4,cuadro_2_0,cuadro_2_1,cuadro_2_2,
-                        cuadro_2_3,cuadro_2_4,cuadro_3_0,cuadro_3_1,cuadro_3_2,cuadro_3_3,cuadro_3_4,cuadro_4_0,cuadro_4_1,cuadro_4_2,cuadro_4_3,cuadro_4_4,cuadro_5_0,
-                        cuadro_5_1,cuadro_5_2,cuadro_5_3,cuadro_5_4,cuadro_6_0,cuadro_6_1,cuadro_6_2,cuadro_6_3,cuadro_6_4,cuadro_7_0,cuadro_7_1,cuadro_7_2,cuadro_7_3,
-                        cuadro_7_4,cuadro_8_0,cuadro_8_1,cuadro_8_2,cuadro_8_3,cuadro_8_4]
 
-y0 = c[0].coordsy_get()
-x0 = c[0].coordsx_get()
-y1 = c[1].coordsy_get()
-x1 = c[1].coordsx_get()
-
-def lanza_agua(estado,x,y):
-    if estado.estado_get() == cuadro_o_con_water_rooks:
-        ventana.blit(agua,(x,y))
-        sleep(1)
-        return lanza_agua(estado,x,y)
-    else:
-        return 0
 
 
 #########################################################################################################################################################################
@@ -310,22 +290,19 @@ while True:
     #############################################
     #proyectiles
     if c[0].estado_get() == cuadro_o_con_water_rooks:
-        estado = c[0]
-        if y0 < 400:
-            y0 +=1
-            hilo2 = Thread(target=lanza_agua, args=(estado,x0, y0))
-            hilo2.start()       
+        c[0].disparos(x0,y0,bola_de_agua)
     if c[0].estado_get() == cuadro_oscuro:
         y0 = c[0].coordsy_get()
-    if c[1].estado_get() == cuadro_o_con_water_rooks:
-        estado = c[1]
-        if y1 < 400:
-            y1 +=1
-            hilo2 = Thread(target=lanza_agua, args=(estado,x1, y1))
-            hilo2.start()       
-    if c[1].estado_get() == cuadro_oscuro:
-        y1 = c[1].coordsy_get()
 
+
+    if len(c[0].lista_disparo)>0:
+        for ele in c[0].lista_disparo:
+            ele.direccion() 
+            ele.disparo(ventana)
+
+            if ele.rect.top > 400:
+                c[0].lista_disparo.remove(ele)
+                
     
     
     #llama al cursor
